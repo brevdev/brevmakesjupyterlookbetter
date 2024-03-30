e57eca7e4e9b47cc7e75f3ed49c3dfcb5328e849
Nader Khalil <nader@brev.dev>
2024-03-29 21:45:54 -0700

    init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the brevmakesjupyterlookbetter extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'brevmakesjupyterlookbetter:plugin',
+  description: 'brev.dev's dark and light mode theme',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension brevmakesjupyterlookbetter is activated!');
+    const style = 'brevmakesjupyterlookbetter/index.css';
+
+    manager.register({
+      name: 'brevmakesjupyterlookbetter',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
